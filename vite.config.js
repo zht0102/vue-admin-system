06b22a4c524e178173c02dd4b3dcc089916afc26
zhanghaitao <zhanghaitao@zhongan.com>
2025-03-19 15:46:53 +0800

    feat: change base path

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,17 @@ import path from 'path'
 import { viteMockServe } from 'vite-plugin-mock'
 
 export default defineConfig({
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        chunkFileNames: 'assets/js/[name]-[hash].js',
+        entryFileNames: 'assets/js/[name]-[hash].js',
+        assetFileNames: 'assets/[ext]/[name]-[hash].[ext]'
+      }
+    }
+  },
   base: process.env.NODE_ENV === 'production' ? '/vue-admin-system/' : '/',
   plugins: [
     vue(),
